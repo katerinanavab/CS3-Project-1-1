169dad3e473ce8a4b7006f04dffff8c05cae4ecd
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-20 14:33:39 +0000

    updated settings and main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+# You can initialize important data at the top of the program!
+score = 0
+trivia = [{ 
+           'question': "Who is the mayor of Pelican town?",
+           'answer': 'Lewis',
+           'options': ['Lewis', 'You', 'Marnie', 'Pierre']
+           },
+          { 
+           'question': "It's Abigail's birthday! Pick a loved gift:",
+           'answer': 'Amethyst',
+           'options': ['Amethyst', 'Holly', 'Sugar', 'Quartz']
+           },
+          { 
+           'question': "Which ore is found on floor 40 of the mines?",
+           'answer': 'Silver',
+           'options': ['Silver', 'Gold', 'Copper', 'Iridium']
+           },
+          ]
+
+
+# Main method for game loop
 def main():
-    print("hello world")
+    print("Let's play 🐓 Stardew Valley 🚜 Trivia!")
+    #choice = input("Your answer:")
+    #print(choice)
 
 
 if __name__ == "__main__":
